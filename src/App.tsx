76f5f4e747c2d9c5199c7257d9c680c624bca7c4
João Luft <joaoluft26@gmail.com>
2024-04-25 16:28:59 -0300

    Feat: Tela de login com background interativo

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,20 @@
+import { Login } from "./components/Login";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { GlobalStyles } from "./themes/GlobalStyles";
 
 export const App = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" />
-        <Route path="/profile" />
-        <Route path="/login" />
-        <Route path="/anime/:id" />
-        <Route path="*" />
-      </Routes>
-    </Router>
-  )
-}
+    <>
+      <GlobalStyles />
+      <Router>
+        <Routes>
+          <Route path="/" />
+          <Route path="/profile" />
+          <Route path="/login" element={<Login />} />
+          <Route path="/anime/:id" />
+          <Route path="*" />
+        </Routes>
+      </Router>
+    </>
+  );
+};
