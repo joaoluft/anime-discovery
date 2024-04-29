c3a11fbd51da7ab52395baf4595ce2ce3adfb5dd
João Luft <joaoluft26@gmail.com>
2024-04-29 16:38:42 -0300

    Feat: Estilo de lista de top animes

--- src/components/AnimeList/index.tsx
+++ src/components/AnimeList/index.tsx
@@ -1,24 +1,18 @@
-import { useAnimeList } from "../../hooks/useAnimeList"  
+import { useAnimeList } from "../../hooks/useAnimeList";
+import { Anime } from "../Anime";
+import { Container } from "./styles";
 export const AnimeList = () => {
-    const { animeList } = useAnimeList();
-    console.log(animeList)
-    const renderAnimes = () => {
-        return animeList.data.map(anime => {
-            console.log(anime)
-            return (
-                <>
-                    <div>{anime.title}</div>
-                    <div>{anime.mal_id}</div>
-                    <img src={anime.images.webp.image_url}></img>
-                </>
-            )
-        }
-        )
-    }
+  const { animeList } = useAnimeList();
+  console.log(animeList);
+  const renderAnimes = () => {
+    return animeList.data.map((anime) => (
+      <Anime
+        image={anime.images.webp.image_url}
+        rank={anime.rank}
+        name={anime.title}
+      />
+    ));
+  };
 
-    return (
-        <div>
-            {renderAnimes()}
-        </div>
-    );
-}
+  return <Container>{renderAnimes()}</Container>;
+};
